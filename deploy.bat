be5e08a04b183d903055d25c3f4bee7cca18ca26
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-02-02 18:34:06 -0500

    build update

--- deploy.bat
+++ deploy.bat
@@ -8,11 +8,18 @@ rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
 set H=%KSPDIR%
-set GAMEDIR=TWR1
-set GAMEDATA="GameData\Diazo"
+set GAMEDIR=Diazo\TWR1
+set GAMEDATA="GameData"
 set VERSIONFILE=%GAMEDIR%.version
 
+set DP0=r:\dp0\kspdev
+
 copy /Y "%1%2" "%GAMEDATA%\%GAMEDIR%\Plugins"
+copy /Y "%1%3".pdb "%GAMEDATA%\%GAMEDIR%\Plugins"
+
 copy /Y %VERSIONFILE% %GAMEDATA%\%GAMEDIR%
 
-xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\%GAMEDATA%\%GAMEDIR%"
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%H%\GameData\%GAMEDIR%"
+xcopy /y /s /I %GAMEDATA%\%GAMEDIR% "%DP0%\GameData\%GAMEDIR%"
+
+pause
